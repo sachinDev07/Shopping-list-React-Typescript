8747a1cd671f61da531171b5e4660dcfb77eb701
sachinDev07 <sachinkrj077@gmail.com>
2024-04-30 21:01:52 +0530

    Added basic input tag and a button

--- src/components/ShoppingList/ShoppingList.tsx
+++ src/components/ShoppingList/ShoppingList.tsx
@@ -1,12 +1,15 @@
 import Header from "../header/Header";
-
+import InputItem from "../InputItem/InputItem";
 
 function ShoppingList() {
-    return (
-        <>
-            <Header />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <div className="flex items-center justify-center">
+        <InputItem />
+      </div>
+    </>
+  );
 }
 
 export default ShoppingList;
